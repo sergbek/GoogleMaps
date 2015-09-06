cdfbb04e0263281fbf49a21d92a68d95ca355e2f
sergbek <sergbek@ukr.net>
2015-09-06 23:18:35 +0300

    add class MyLocation

--- app/src/main/java/com/example/sergbek/googlemapsl18/activity/MainActivity.java
+++ app/src/main/java/com/example/sergbek/googlemapsl18/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.sergbek.googlemapsl18;
+package com.example.sergbek.googlemapsl18.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.sergbek.googlemapsl18.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_AM);
         mapFragment.getMapAsync(this);
     }
 
@@ -24,8 +26,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap=googleMap;
+        mMap.setTrafficEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.getUiSettings().setCompassEnabled(true);
         mMap.setMyLocationEnabled(true);
     }
+
 }
---
+++ app/src/main/java/com/example/sergbek/googlemapsl18/fragment/MyLocationFragment.java
@@ -0,0 +1,9 @@
+package com.example.sergbek.googlemapsl18.fragment;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by Sergbek on 06.09.2015.
+ */
+public class MyLocationFragment extends DialogFragment {
+}
